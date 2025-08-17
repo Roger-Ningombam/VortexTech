3adc486c86323bae03b1b50af43337ea19e0f204
Roger-Ningombam <roger.ningombam1m@gmail.com>
2025-08-17 14:40:39 +0530

    Some congig change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // This tells Vite where the deployed project will live.
+  base: '/VortexTech/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['three', '@react-three/fiber', '@react-three/drei']
